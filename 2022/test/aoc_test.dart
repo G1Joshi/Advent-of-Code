65e9999cd2e051daf1e74ff242d8d336fecd5ed2
Jeevan Joshi <joshijeevan98@gmail.com>
2022-12-01 21:59:17 +0530

    test day01

--- 2022/test/aoc_test.dart
+++ 2022/test/aoc_test.dart
@@ -2,5 +2,13 @@ import 'package:aoc/aoc.dart';
 import 'package:test/test.dart';
 
 void main() {
-  
+  group('Day01', () {
+    final day = Day01();
+    test('Part1', () {
+      expect(day.part1(), 66186);
+    });
+    test('Part2', () {
+      expect(day.part2(), 196804);
+    });
+  });
 }
