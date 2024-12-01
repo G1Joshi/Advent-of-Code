1ebcd2ff9a520f0eb27ad29d79332791e528d0ac
Jeevan Joshi <joshijeevan98@gmail.com>
2024-12-01 06:25:30 +0000

    test day01

--- 2024/Tests/AOCTests.swift
+++ 2024/Tests/AOCTests.swift
@@ -9,4 +9,19 @@
 import Testing
 
 @Suite("Tests")
-struct AOCTests {}
+struct AOCTests {
+    @Suite("Day01 Tests")
+    struct Day01Tests {
+        @Test("Part 1")
+        func testPart1() async throws {
+            let challenge = Day01()
+            #expect(challenge.part1() == 2166959)
+        }
+
+        @Test("Part 2")
+        func testPart2() async throws {
+            let challenge = Day01()
+            #expect(challenge.part2() == 23741109)
+        }
+    }
+}
